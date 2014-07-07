855ffa0096983bb9ee9768c9e9f041f759246665
mcberros <mcberros@gmail.com>
2014-07-07 14:36:48 +0200

    Added the attributes that will be shown in the company page. Also added a public get_company method that will be accesed from the company controller to show the information of this company. Added method to find out if the company data from crunch base has foundation date or a team of people. The people will be the information of the board of advisors and the current team; the information of the past team doesnt appear. Added public method to know if the company object has people or a foundation date; these methods will be used in the view

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,12 +2,16 @@ class Company
 
 	COMPANIES_PER_PAGE = 10
 
-	attr_accessor :name, :description, :image
+	attr_accessor :name, :description, :image, :crunch_url, :founded, :people
 
 	def initialize(company_data)
+		@crunch_hostname = company_data['metadata']['www_path_prefix']
 		@name = company_data['data']['properties']['name']
 		@description = has_short_description?(company_data) ? get_short_description(company_data) : ''
 		@image = has_primary_image?(company_data) ? get_image_url(company_data) : ''
+		@crunch_url = "#{@crunch_hostname}organization/#{company_data['data']['properties']['permalink']}"
+		@founded = has_founded?(company_data) ? get_foundation_date(company_data) : ''
+		@people = (has_board?(company_data) || has_current_team?(company_data)) ? get_people(company_data) : nil
 	end
 
 	def self.page(model_page)
@@ -32,8 +36,22 @@ class Company
 		max_pages = (total_companies/COMPANIES_PER_PAGE.to_f).ceil
 	end
 
+	def self.get_company(permalink)
+		crunch_access = CrunchAccessService.new
+		company = crunch_access.get_company(permalink)
+		Company.new(company)
+	end
+
 	def has_image?
 		!@image.empty?
+	end
+
+	def has_foundation_date?
+		!@founded.empty?
+	end
+
+	def has_people?
+		!@people.nil?
 	end
 
 	private
@@ -50,6 +68,36 @@ class Company
 			!company_data['data']['relationships'].empty?
 		end
 
+		def has_board?(company_data)
+	  	has_relationships?(company_data) &&
+	  	 !company_data['data']['relationships']['board_members_and_advisors'].nil? &&
+	  	 !company_data['data']['relationships']['board_members_and_advisors']['paging']['total_items'].nil? &&
+	  	  company_data['data']['relationships']['board_members_and_advisors']['paging']['total_items'] > 0
+	  end
+
+		def has_current_team?(company_data)
+	  	has_relationships?(company_data) &&
+	  	 !company_data['data']['relationships']['current_team'].nil? &&
+	  	 !company_data['data']['relationships']['current_team']['paging']['total_items'].nil? &&
+	  	  company_data['data']['relationships']['current_team']['paging']['total_items'] > 0
+	  end
+
+		def get_people(company_data)
+			board = []
+			team = []
+			if has_board?(company_data)
+				board = company_data['data']['relationships']['board_members_and_advisors']['items'].map do |board_member|
+					Person.new(@crunch_hostname, board_member)
+				end
+			end
+			if has_current_team?(company_data)
+				team = company_data['data']['relationships']['current_team']['items'].map do |team_member|
+					Person.new(@crunch_hostname, team_member)
+				end
+			end
+			board.concat(team)
+		end
+
 		def has_primary_image?(company_data)
 			has_relationships?(company_data) &&
 			 !company_data['data']['relationships']['primary_image'].nil?
@@ -57,5 +105,14 @@ class Company
 
 		def get_image_url(company_data)
 			"#{company_data['metadata']['image_path_prefix']}/#{company_data['data']['relationships']['primary_image']['items'][0]['path']}"
+		end
+
+		def has_founded?(company_data)
+			!company_data['data']['properties']['founded_on'].nil? &&
+			 !company_data['data']['properties']['founded_on'].empty?
+		end
+
+		def get_foundation_date(company_data)
+			company_data['data']['properties']['founded_on']
 		end
 end
