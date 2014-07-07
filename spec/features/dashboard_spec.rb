e7a604e139cd4465e5dc62a18a8e72c2d52216e7
mcberros <mcberros@gmail.com>
2014-07-07 15:53:50 +0200

    We need to add the stubs for the product accesses and also we add a test for the product list

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -2,16 +2,22 @@ require 'spec_helper'
 
 describe 'dashboard' do
 
-	context 'with only one page of companies' do
+	context 'with only one page of companies and products' do
 		it 'No Prev or Next' do
 			stub_one_company_page
 
 			stub_one_company
 
+			stub_one_product_page
+
+			stub_one_product
+
 			visit '/dashboard'
 
 			expect(page).to have_content("Keen IO")
 			expect(page).to have_content("Keen IO is a custom analytics back-end for modern developers.")
+			expect(page).to have_content("Basecamp")
+			expect(page).to have_content("Basecamp is a project management ")
 			expect(page).not_to have_content("Prev")
 			expect(page).not_to have_content("Next")
 		end
@@ -23,6 +29,10 @@ describe 'dashboard' do
 			stub_three_company_pages
 
 			stub_each_company_for_the_first_page
+
+			stub_one_product_page
+
+			stub_one_product
 		end
 
 		it 'first page only sees Next' do
@@ -33,14 +43,48 @@ describe 'dashboard' do
 		end
 
 		it 'second page sees Prev and Next' do
-			visit '/dashboard?page_companies=2'
+			visit '/dashboard?page_companies=2&page_products=1'
 
 			expect(page).to have_content('Prev')
 			expect(page).to have_content('Next')
 		end
 
 		it 'thirsd page sees only Prev' do
-			visit '/dashboard?page_companies=3'
+			visit '/dashboard?page_companies=3&page_products=1'
+
+			expect(page).to have_content('Prev')
+			expect(page).not_to have_content('Next')
+		end
+	end
+
+	context 'with three pages of products' do
+
+		before(:each) do
+			stub_one_company_page
+
+			stub_one_company
+
+			stub_three_product_pages
+
+			stub_each_product_for_the_first_page
+		end
+
+		it 'first page only sees Next' do
+			visit '/dashboard'
+
+			expect(page).not_to have_content('Prev')
+			expect(page).to have_content('Next')
+		end
+
+		it 'second page sees Prev and Next' do
+			visit '/dashboard?page_companies=1&page_products=2'
+
+			expect(page).to have_content('Prev')
+			expect(page).to have_content('Next')
+		end
+
+		it 'thirsd page sees only Prev' do
+			visit '/dashboard?page_companies=1&page_products=3'
 
 			expect(page).to have_content('Prev')
 			expect(page).not_to have_content('Next')
