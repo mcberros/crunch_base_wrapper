e1baa96b5d6cba6bac35724f9a9b5202f1ecc699
mcberros <mcberros@gmail.com>
2014-07-07 15:31:18 +0200

    Added the part for the products. Its similar to the company code. There should be now also a parameter to control the pagination in the product list

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -8,5 +8,14 @@ class DashboardsController < ApplicationController
 			@page_companies = params[:page_companies].to_i
 			@companies = Company.page(params[:page_companies])
 		end
+
+		@max_product_pages = Product.get_max_pages
+		unless params[:page_products]
+			@page_products =1
+			@products = Product.page(@page_products)
+		else
+			@page_products = params[:page_products].to_i
+			@products = Product.page(params[:page_products])
+		end
 	end
 end
