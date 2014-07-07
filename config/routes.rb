937b40bdcd4737a3e2c1915b4b4537799d7da726
mcberros <mcberros@gmail.com>
2014-07-07 12:40:09 +0200

    Added a new route for the dashboards controller. Only the show method will be allowed. The root of the application will be the show method of this controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  resource :dashboard, only: :show
+
+  root 'dashboards#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
