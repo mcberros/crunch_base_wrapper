a13fa3249f03f2ae6fb9d94754b935b685ad454c
mcberros <mcberros@gmail.com>
2014-07-22 21:43:56 +0200

    Every crunh base uri parameter in a constant in the Service

--- app/services/crunch_access_service.rb
+++ app/services/crunch_access_service.rb
@@ -7,24 +7,26 @@ class CrunchAccessService
 	ORGANIZATION_URL = "http://api.crunchbase.com/v/2/organization"
 	PRODUCTS_URL = "http://api.crunchbase.com/v/2/products"
 	PRODUCT_URL = "http://api.crunchbase.com/v/2/product"
+	USER_KEY = "e314b958fbba5e68116283cf9f8cd96e"
+	ORDER = "created_at+DESC"
 
 	def get_companies(page=1)
-		uri = URI("#{ORGANIZATIONS_URL}?user_key=e314b958fbba5e68116283cf9f8cd96e&page=#{page}&order=created_at+DESC")
+		uri = URI("#{ORGANIZATIONS_URL}?user_key=#{USER_KEY}&page=#{page}&order=#{ORDER}")
 		JSON.parse(Net::HTTP.get(uri))
 	end
 
 	def get_company(permalink)
-		uri = URI("#{ORGANIZATION_URL}/#{permalink}?user_key=e314b958fbba5e68116283cf9f8cd96e")
+		uri = URI("#{ORGANIZATION_URL}/#{permalink}?user_key=#{USER_KEY}")
 		JSON.parse(Net::HTTP.get(uri))
 	end
 
 	def get_products(page=1)
-		uri = URI("#{PRODUCTS_URL}?user_key=e314b958fbba5e68116283cf9f8cd96e&page=#{page}&order=created_at+DESC")
+		uri = URI("#{PRODUCTS_URL}?user_key=#{USER_KEY}&page=#{page}&order=#{ORDER}")
 		JSON.parse(Net::HTTP.get(uri))
 	end
 
 	def get_product(permalink)
-		uri = URI("#{PRODUCT_URL}/#{permalink}?user_key=e314b958fbba5e68116283cf9f8cd96e")
+		uri = URI("#{PRODUCT_URL}/#{permalink}?user_key=#{USER_KEY}")
 		JSON.parse(Net::HTTP.get(uri))
 	end
 end
