067842f16376eed4f238e4d604f306dff04e3850
mcberros <mcberros@gmail.com>
2014-07-22 22:28:46 +0200

    We extract configuration for the access to to crunch base to a file

--- app/services/crunch_access_service.rb
+++ app/services/crunch_access_service.rb
@@ -3,30 +3,23 @@ require 'json'
 
 class CrunchAccessService
 
-	ORGANIZATIONS_URL = "http://api.crunchbase.com/v/2/organizations"
-	ORGANIZATION_URL = "http://api.crunchbase.com/v/2/organization"
-	PRODUCTS_URL = "http://api.crunchbase.com/v/2/products"
-	PRODUCT_URL = "http://api.crunchbase.com/v/2/product"
-	USER_KEY = "e314b958fbba5e68116283cf9f8cd96e"
-	ORDER = "created_at+DESC"
-
 	def get_companies(page=1)
-		uri = URI("#{ORGANIZATIONS_URL}?user_key=#{USER_KEY}&page=#{page}&order=#{ORDER}")
+		uri = URI("#{CRUNCH_BASE_CONFIG['organizations_url']}?user_key=#{CRUNCH_BASE_CONFIG['user_key']}&page=#{page}&order=#{CRUNCH_BASE_CONFIG['order']}")
 		JSON.parse(Net::HTTP.get(uri))
 	end
 
 	def get_company(permalink)
-		uri = URI("#{ORGANIZATION_URL}/#{permalink}?user_key=#{USER_KEY}")
+		uri = URI("#{CRUNCH_BASE_CONFIG['organization_url']}/#{permalink}?user_key=#{CRUNCH_BASE_CONFIG['user_key']}")
 		JSON.parse(Net::HTTP.get(uri))
 	end
 
 	def get_products(page=1)
-		uri = URI("#{PRODUCTS_URL}?user_key=#{USER_KEY}&page=#{page}&order=#{ORDER}")
+		uri = URI("#{CRUNCH_BASE_CONFIG['products_url']}?user_key=#{CRUNCH_BASE_CONFIG['user_key']}&page=#{page}&order=#{CRUNCH_BASE_CONFIG['order']}")
 		JSON.parse(Net::HTTP.get(uri))
 	end
 
 	def get_product(permalink)
-		uri = URI("#{PRODUCT_URL}/#{permalink}?user_key=#{USER_KEY}")
+		uri = URI("#{CRUNCH_BASE_CONFIG['product_url']}/#{permalink}?user_key=#{CRUNCH_BASE_CONFIG['user_key']}")
 		JSON.parse(Net::HTTP.get(uri))
 	end
 end
---
+++ config/initializers/crunch_base.rb
@@ -0,0 +1,3 @@
+CRUNCH_BASE_CONFIG_PATH="#{Rails.root}/config/crunch_base_config.yml"
+
+CRUNCH_BASE_CONFIG = YAML.load_file(CRUNCH_BASE_CONFIG_PATH)
